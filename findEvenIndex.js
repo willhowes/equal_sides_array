0eb9b1caa3f388168d088dc4630b9ea5a3d554ec
eldenhowes <williameldenhowes@gmail.com>
2019-10-15 08:55:20 +0100

    Refactored

--- findEvenIndex.js
+++ findEvenIndex.js
@@ -3,18 +3,17 @@ const findEvenIndex = array => {
   const halfOfTotal = arrayTotal / 2;
   let i = 0;
   let runningTotal = 0;
+
   while (runningTotal < halfOfTotal) {
     runningTotal = runningTotal + array[i];
     i++;
   }
+
   let middleIndex = i - 1;
   const firstHalfTotal = array.slice(0, middleIndex).reduce((a, b) => a + b, 0);
   const secondHalfTotal = array
     .slice(middleIndex + 1, array.length)
     .reduce((a, b) => a + b, 0);
-  console.log(`----${array}-----`);
-  console.log(`FIRST HALF: ${firstHalfTotal}`);
-  console.log(`SECOND HALF: ${secondHalfTotal}`);
 
   if (firstHalfTotal === secondHalfTotal) {
     return middleIndex;
