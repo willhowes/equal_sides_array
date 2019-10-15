36ba8e855ce062336fe919631e68dbfcd908c114
eldenhowes <williameldenhowes@gmail.com>
2019-10-15 08:41:41 +0100

    Refactored

--- findEvenIndex.js
+++ findEvenIndex.js
@@ -5,14 +5,9 @@ const findEvenIndex = array => {
   let runningTotal = 0;
   while (runningTotal < halfOfTotal) {
     runningTotal = runningTotal + array[i];
-    console.log(`INDEX: ${i}`);
-    console.log(`RUNNING TOT: ${runningTotal}`);
     i++;
   }
-  console.log("---END OF TEST----");
   return i - 1;
-  // let halfway = array.length / 2;
-  // return Math.floor(halfway);
 };
 
 module.exports = findEvenIndex;
